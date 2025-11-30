1dfeea6f475586dcec67be356ff7e108c2053638
kjnq5634 <kjnq5634@leeds.ac.uk>
2025-11-30 13:04:08 +0000

    Initial commit

---
+++ funcs.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "funcs.h"
+
+void menu_item_1(void) {
+    printf("\n>> Menu 1\n");
+    printf("\nSome code here does something useful\n");
+    /* you can call a function from here that handles menu 1 */
+}
+
+void menu_item_2(void) {
+    printf("\n>> Menu 2\n");
+    printf("\nSome code here does something useful\n");
+    /* you can call a function from here that handles menu 2 */
+}
+
+void menu_item_3(void) {
+    printf("\n>> Menu 3\n");
+    printf("\nSome code here does something useful\n");
+    /* you can call a function from here that handles menu 3 */
+}
+
+void menu_item_4(void) {
+    printf("\n>> Menu 4\n");
+    printf("\nSome code here does something useful\n");
+    /* you can call a function from here that handles menu 4 */
+}
